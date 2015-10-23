e31e5a9538106acb645dca045917f3039b22b5cd
stiraei@ryerson.ca <stiraei@scherzo.ee.ryerson.ca>
2015-10-23 17:08:56 -0400

    added comment

---
+++ src/hello.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : hello.cpp
+// Author      : SusanneT
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hw" << endl; // prints hw
+	return 0;
+}
+//hjhj
